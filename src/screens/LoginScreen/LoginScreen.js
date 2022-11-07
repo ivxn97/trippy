e5c1f0b0cf8e77158285f71157e4833f284660fd
ivxn97 <kudoboi@hotmail.com>
2022-11-08 00:09:27 +0800

    changes

---
+++ src/screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import styles from './styles';
+import { doc, getDoc } from "firebase/firestore";
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { db } from '../../../config';
+
+export default function LoginScreen({navigation}) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onFooterLinkPress = () => {
+        navigation.navigate('Registration')
+    }
+
+    const onLoginPress = () => {
+        signInWithEmailAndPassword(email, password)
+        .then(async (userCredential) => {
+            try {
+                const docRef = doc(db, "users", email);
+                const docSnap = await getDoc(docRef);
+
+                if (docSnap.exists()) {
+                    console.log("Login Success");
+                }
+                else {
+                    console.log("Login failed");
+                }
+                //console.log("Document written with ID: ", docRef.id);
+                navigation.navigate('Home', {user: auth})
+            }
+            catch (e) {
+                console.log("Error adding document: ", e);
+            }
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+    }
+
+    return (
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+                <Image
+                    style={styles.logo}
+                    source={require('../../../assets/icon.png')}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='E-mail'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Password'
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => onLoginPress()}>
+                    <Text style={styles.buttonTitle}>Log in</Text>
+                </TouchableOpacity>
+                <View style={styles.footerView}>
+                    <Text style={styles.footerText}>Don't have an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Sign up</Text></Text>
+                </View>
+            </KeyboardAwareScrollView>
+        </View>
+    )
+}
---
+++ src/screens/RegistrationScreen/RegistrationScreen.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { doc, setDoc } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import styles from './styles';
+import { db } from '../../../config';
+
+export default function RegistrationScreen({navigation}) {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    // Implement password length check, minimum length of 6
+    const onFooterLinkPress = () => {
+        navigation.navigate('Login')
+    }
+
+    const onRegisterPress = () => {
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(async (userCredential) => {
+            try {
+                const uid = userCredential.user.uid
+                const docRef = await setDoc(doc(db, "users", email), {
+                    first: firstName,
+                    last: lastName,
+                    email: email,
+                    id: uid
+                });
+                //console.log("Document written with ID: ", docRef.id);
+                navigation.navigate('Home', {user: auth})
+            }
+            catch (e) {
+                console.log("Error adding document: ", e);
+            }
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+    }
+
+    return (
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+                <Image
+                    style={styles.logo}
+                    source={require('../../../assets/icon.png')}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='First Name'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setFirstName(text)}
+                    value={firstName}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Last Name'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setLastName(text)}
+                    value={lastName}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='E-mail'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Password'
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Confirm Password'
+                    onChangeText={(text) => setConfirmPassword(text)}
+                    value={confirmPassword}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => onRegisterPress()}>
+                    <Text style={styles.buttonTitle}>Create account</Text>
+                </TouchableOpacity>
+                <View style={styles.footerView}>
+                    <Text style={styles.footerText}>Already got an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Log in</Text></Text>
+                </View>
+            </KeyboardAwareScrollView>
+        </View>
+    )
+}

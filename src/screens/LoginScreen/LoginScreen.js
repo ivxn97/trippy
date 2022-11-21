99e281bc6c611331377be9ce99fd2e070ae12385
ivxn97 <kudoboi@hotmail.com>
2022-11-21 16:54:19 +0800

    big commits

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -2,23 +2,28 @@ import React, { useState } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
-import { doc, getDoc } from "firebase/firestore";
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { doc, getDoc, collection, query, where, getDocs } from "firebase/firestore";
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
 import { db } from '../../../config';
+import { FireSQL } from 'firesql';
 
 export default function LoginScreen({navigation}) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const fireSQL = new FireSQL(db);
+
     const onFooterLinkPress = () => {
         navigation.navigate('Registration')
     }
+    const auth = getAuth();
 
     const onLoginPress = () => {
-        signInWithEmailAndPassword(email, password)
+        signInWithEmailAndPassword(auth, email, password)
         .then(async (userCredential) => {
             try {
-                const docRef = doc(db, "users", email);
+                const user = userCredential.user;
+                /*const docRef = doc(db, "users", email);
                 const docSnap = await getDoc(docRef);
 
                 if (docSnap.exists()) {
@@ -28,7 +33,40 @@ export default function LoginScreen({navigation}) {
                     console.log("Login failed");
                 }
                 //console.log("Document written with ID: ", docRef.id);
-                navigation.navigate('Home', {user: auth})
+                navigation.navigate('Profile', {user: auth}) 
+                const q = query(collection(db, "users"), where("email", "==", email));
+                const querySnapshot = await getDocs(q);
+                querySnapshot.forEach((doc) => {
+
+                })*/
+
+                /*const queryPromise = fireSQL.query(`SELECT role FROM users WHERE email = `email);
+                queryPromise.then(users => {
+                    for (const user of users) {
+                        if (`${user.role}` == 'Registered User') {
+                            const userRole = 'Registered User';
+                            navigation.navigate('Home', {user: userRole})
+                        }
+                        else if (`${user.role}` == 'LOL') {
+                            const userRole = 'LOL';
+                            navigation.navigate('Home', {user: userRole})
+                        }
+                        else if (`${user.role}` == 'Business Owner') {
+                            const userRole = 'Business Owner';
+                            navigation.navigate('Profile', {user: userRole})
+                        }
+                        else if (`${user.role}` == 'Admin') {
+                            const userRole = 'Admin';
+                            navigation.navigate('AdminScreen', {user: userRole})
+                        }
+                        else {
+                            console.log ("error");
+                        }
+                    }
+                })*/
+
+                const usr = db.collection('users');
+                usr.where('email', '==', email).where('role', '==', 'Registered User').get();
             }
             catch (e) {
                 console.log("Error adding document: ", e);

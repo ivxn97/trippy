bf286b7bda3e1e29d31b74bd82e1d07455057dbd
raymondlee977 <raymond030797@gmail.com>
2022-12-10 20:23:38 +0800

    Deals
    List and Screen

--- src/screens/DealsScreen/DealsList.js
+++ src/screens/DealsScreen/DealsList.js
@@ -1,11 +1,68 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, FlatList, View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { doc, getDoc, collection, query, where, getDocs } from "firebase/firestore";
+import { db } from '../../../config';
+import { TouchableHighlight } from 'react-native-gesture-handler';
+import styles from './styles';
 
-export default function AttractionDeals ( {navigation} ) {
-    return (
-        <View>
-            
+export default function AttractionDeals( { navigation }) {
+  const [loading, setLoading] = useState(true); // Set loading to true on component mount
+  const [deals, setDeals] = useState([]); // Initial empty array of attractions
+
+  //List
+  navigation.addListener('willFocus', () => {
+    
+  })
+
+  useEffect(async () => {
+    const querySnapshot = await getDocs(collection(db, "deals"));
+        querySnapshot.forEach(documentSnapshot => {
+          deals.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+
+        setDeals(deals);
+        setLoading(false);
+      },[]);
+  
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  return (
+    <View>
+    <TextInput
+        style={styles.inputSearch}
+        placeholder='search'
+        placeholderTextColor="#aaaaaa"
+        underlineColorAndroid="transparent"
+        autoCapitalize="sentences"
+    />
+    <View style={{ flexDirection:"row", justifyContent: 'flex-end' }}>
+        <TouchableOpacity style={styles.buttonListLeft}>
+          <Text style={styles.buttonSmallListText}>Sort</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonListRight}>
+          <Text style={styles.buttonSmallListText}>Filter</Text>
+        </TouchableOpacity>
+    </View>
+    <FlatList
+      data={deals}
+      extraData={deals}
+      renderItem={({ item }) => (
+        <TouchableHighlight
+        underlayColor="#C8c9c9"
+        onPress={() => {navigation.navigate('Deal detail', {name: item.dealname, dealType: item.type, 
+        code: item.code, description: item.description, quantity: item.quantity, TNC: item.TNC})}}>
+        <View style={styles.list}>
+          <Text>{item.dealname}</Text>
+          <Text>{item.quantity}%</Text>
         </View>
-    )
+        </TouchableHighlight>
+      )}
+    />
+    </View>
+  );
 }
-
--- src/screens/DealsScreen/DealsScreen.js
+++ src/screens/DealsScreen/DealsScreen.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react'
+import { Image, Text, TextInput, TouchableOpacity, View, ScrollView, StyleSheet } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import styles from './styles';
 
-export default function DealsScreen ( {navigation} ) {
+export default function DealsScreen ( {route, navigation} ) {
+
+    const {name, dealType, code, description, quantity, TNC} = route.params;
+
     return (
-        <View>
-            <Button
-            title ="Attraction"
-            onPress={() =>
-                navigation.navigate('Attraction Deals')
-            }
+        <View style={styles.detailsContainer}>
+            <Text style={styles.Heading}>{JSON.stringify(name).replace(/"/g,"")}</Text>
+            <Image
+                style={styles.imageDetailsPlaceholder}
+                source={require('../../../assets/image-placeholder-large.jpg')}
             />
+            <View style={{ flexDirection:"row" }}>
+                <TouchableOpacity style={styles.buttonSmall}>
+                        <Text style={styles.buttonSmallText}>Save</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonSmall}>
+                        <Text style={styles.buttonSmallText}>Add To Itinerary</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonSmall}>
+                        <Text style={styles.buttonSmallText}>Share</Text>
+                </TouchableOpacity>
+            </View>
+            <Text style={styles.textNB}>Discount: {JSON.stringify(quantity).replace(/"/g,"")}%</Text>
+            <Text style={styles.textNB}>Code: {JSON.stringify(code).replace(/"/g,"")}</Text>
+            <Text style={styles.textNB}>Type: {JSON.stringify(dealType).replace(/"/g,"")}</Text>
+            <Text style={styles.textNB}>Description: {JSON.stringify(description).replace(/"/g,"")}</Text>
+            <Text style={styles.textNB}>Terms & Conditions: {JSON.stringify(TNC).replace(/"/g,"")}</Text>
+            <View style={{ flexDirection:"row", justifyContent: 'flex-end' }}>
+                <TouchableOpacity style={styles.buttonSmall}>
+                        <Text style={styles.buttonSmallText}>Read Reviews</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonSmall}>
+                        <Text style={styles.buttonSmallText}>Book</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }

97753dfb1f130a2b89f1dd1b9ced246e0860e726
ivxn97 <kudoboi@hotmail.com>
2022-12-23 04:36:41 +0800

    Working Image View

--- src/screens/Attractions/AttractionView.js
+++ src/screens/Attractions/AttractionView.js
@@ -1,18 +1,57 @@
-import React, { useState } from 'react'
-import { Image, Text, TextInput, TouchableOpacity, View, ScrollView, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { Dimensions, Image, Text, TextInput, TouchableOpacity, View, ScrollView, StyleSheet } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
+import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
+import Carousel from 'react-native-reanimated-carousel';
 
 export default function AttractionView({route, navigation}) {
     const {name, attractionType, price, ageGroup, groupSize, openingTime, closingTime, description,language, TNC} = route.params;
+    const [images, setImages] = useState([]);
+    const storage = getStorage();
+    const width = Dimensions.get('window').width;
+
+    useEffect(() => {
+        const listRef = ref(storage, `attractions/${name.trimEnd()}/images`);
+        Promise.all([
+            listAll(listRef).then((res) => {
+              const promises = res.items.map((folderRef) => {
+                return getDownloadURL(folderRef).then((link) =>  {
+                  return link;
+                });
+              });
+              return Promise.all(promises);
+            })
+          ]).then((results) => {
+            const fetchedImages = results[0];
+            console.log(fetchedImages);
+            setImages(fetchedImages);
+          });
+    }, [])
     
     return (
         <View style={styles.detailsContainer}>
+            <ScrollView>
             <Text style={styles.Heading}>{JSON.stringify(name).replace(/"/g,"")}</Text>
-            <Image
-                style={styles.imageDetailsPlaceholder}
-                source={require('../../../assets/image-placeholder-large.jpg')}
-            />
+            <Carousel width={width}
+                height={width / 2}
+                mode="horizontal"
+                data={images}
+                renderItem={({ item }, index) => (
+                    <View
+                        style={{
+                            flex: 1,
+                            borderWidth: 1,
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Image style={CStyle.carouselStyle} source={{uri: item}}/>
+                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
+                            {index}
+                        </Text>
+                    </View>
+                )}
+                />
             <View style={{ flexDirection:"row" }}>
                 <TouchableOpacity style={styles.buttonSmall}>
                         <Text style={styles.buttonSmallText}>Save</Text>
@@ -42,6 +81,15 @@ export default function AttractionView({route, navigation}) {
                         <Text style={styles.buttonSmallText}>Book</Text>
                 </TouchableOpacity>
             </View>
+            </ScrollView>
         </View>
     )
 }
+
+const CStyle = StyleSheet.create({
+    carouselStyle: {
+        width: 410,
+        height: 250,
+        alignSelf: "center"
+    }
+})

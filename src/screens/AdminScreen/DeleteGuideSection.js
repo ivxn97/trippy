7650bb51756c9d9567caf4e08c969d070b5e5215
IffahIrsalina1603 <119609875+IffahIrsalina1603@users.noreply.github.com>
2023-02-02 12:49:07 +0800

    Add files via upload
    Added Add Edit and Delete Guide Section and Walking Tour (WT) Section

---
+++ src/screens/AdminScreen/DeleteGuideSection.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import { TextInput, View, StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import styles from './styles';
+import { doc, setDoc, deleteDoc } from "firebase/firestore";
+import { db } from '../../../config';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FilteredTextInput } from '../commonFunctions';
+
+export default function DeleteGuideSection ( {route, navigation} ) {
+    const {name, description} = route.params;
+    const [email, setEmail] = useState('');
+    const [role, setRole] = useState('');
+    const [newDescription, setDescription] = useState(description);
+
+    const getEmail = async () => {
+        try {
+            const email = await AsyncStorage.getItem('email');
+            if (email !== null) {
+                setEmail(email);
+            }
+            else {
+                console.log("No Email Selected at Login")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    getEmail();
+
+    const getRole = async () => {
+        try {
+            const role = await AsyncStorage.getItem('role');
+            if (role !== null) {
+                setRole(role);
+                console.log(role)
+            }
+            else {
+                console.log("Account has no role")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    getRole();
+
+    const onConfirmDelete = async () => {
+        if (role == 'Admin') {
+            try {
+                await deleteDoc(doc(db, "guide sections", name));
+                
+                navigation.replace('Admin Page')
+            }
+            catch (e) {
+                console.log("Error deleting guide section: ", e);
+            }
+        }
+        else {
+            alert('Wrong Role')
+        }    
+            
+    }
+
+    return (
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+            <Text style={styles.text}>Sub-Guide Name:</Text>
+            <Text style={styles.text}>{JSON.stringify(name)}</Text>
+           <Text style={styles.text}>Description:</Text>
+            <FilteredTextInput
+                style={styles.desc}
+                placeholder='Description'
+                placeholderTextColor="#aaaaaa"
+                onChangeText={(Text) => setDescription(Text)}
+                value={newDescription}
+                underlineColorAndroid="transparent"
+                autoCapitalize="sentences"
+                multiline
+            />
+            <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => onConfirmDelete()}>
+                        <Text style={styles.buttonTitle}>Delete Sub-Guide</Text>
+                </TouchableOpacity>
+
+            </KeyboardAwareScrollView>
+        </View>
+    )
+}

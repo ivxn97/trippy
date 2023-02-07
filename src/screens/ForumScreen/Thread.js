fab8c15b9ec37f2c78dee5278447d89a921b3856
IffahIrsalina1603 <119609875+IffahIrsalina1603@users.noreply.github.com>
2023-02-07 20:11:11 +0800

    Edit Post button

--- src/screens/ForumScreen/Thread.js
+++ src/screens/ForumScreen/Thread.js
@@ -9,11 +9,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFocusEffect } from '@react-navigation/native';
 
 export default function Thread({route, navigation}) {
-    const {title, description, section} = route.params;
+    const {title, description, section, addedBy} = route.params;
     const [images, setImages] = useState([]);
     const [loading, setLoading] = useState(true);
     const [email, setEmail] = useState('');
     const [registeredButton, setRegisteredButton] = useState(true);
+    const [username, setUsername] = useState('');
     const [reply, setReply] = useState([]); // Initial empty array of replies
 
 
@@ -28,6 +29,24 @@ export default function Thread({route, navigation}) {
             else {
                 console.log("No Email Selected at Login")
                 setRegisteredButton(true);
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const getUsername = async () => {
+        try {
+            const username = await AsyncStorage.getItem('username');
+            if (username !== null) {
+                setUsername(username);
+                if (addedBy == username) {
+                    setDisabledButton(false)
+                }
+            }
+            else {
+                setDisabledButton(true)
+                console.log("No Email Selected at Login")
             }
         } catch (error) {
             console.log(error)
@@ -50,6 +69,7 @@ export default function Thread({route, navigation}) {
 
     useFocusEffect(React.useCallback(async ()=> {
         getEmail();
+        getUsername();
     }, []));    
 
     const onShare = async () => {
@@ -77,39 +97,94 @@ export default function Thread({route, navigation}) {
         )
        }
 
-    return(
-        <View style={styles.threadContainer}>
-            <ScrollView scrollIndicatorInsets={{ top: 1, bottom: 1 }}>
-            <Text style={styles.Heading}>{JSON.stringify(title).replace(/"/g,"")}</Text>
+    if (addedBy == username) {
+        return(
+            <View style={styles.threadContainer}>
+                <ScrollView scrollIndicatorInsets={{ top: 1, bottom: 1 }}>
 
-            {/* details */}
-            <Text style={styles.textNB}>Title: {JSON.stringify(title).replace(/"/g,"")}</Text>
-            <Text style={styles.textNB}>Section: {JSON.stringify(section).replace(/"/g,"")}</Text>
-            <Text style={styles.textNB}>Description: {JSON.stringify(description).replace(/"/g,"")}</Text>
-            
-            {/* buttons */}
-            <View style={{ flexDirection:"row" }}>
-                    <TouchableOpacity style={[styles.buttonSmall, {opacity: registeredButton ? 0.3 : 1}]}
-                    disabled ={registeredButton} onPress={() => onLike()}>
-                            <Text style={styles.buttonSmallText}>Like</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.buttonSmall, {opacity: registeredButton ? 0.3 : 1}]} 
-                    disabled ={registeredButton} onPress={() => {navigation.navigate('Create Reply', {title : title})}}>
-                            <Text style={styles.buttonSmallText}>Comment</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.buttonSmall} onPress={() => onShare()}>
-                            <Text style={styles.buttonSmallText}>Share</Text>
-                    </TouchableOpacity>
-                </View>
+                {/* Header */}
+                <Text style={styles.Heading}>{JSON.stringify(title).replace(/"/g,"")}</Text>
 
-            {/* FlatList -- replies */}
+                {/* buttons */}
+                <View style={{ flexDirection:"row", justifyContent: 'flex-end' }}>
+                <TouchableOpacity style={styles.buttonSmall}  onPress={() => {navigation.navigate('Edit Post', {title : title, description: description,
+                            section: section, addedBy: addedBy})}}>
+                    <Text style={styles.buttonSmallListText}>Edit</Text>
+                </TouchableOpacity>
+            </View>
+
+                {/* details */}
+                <Text style={styles.textNB}>Title: {JSON.stringify(title).replace(/"/g,"")}</Text>
+                <Text style={styles.textNB}>Section: {JSON.stringify(section).replace(/"/g,"")}</Text>
+                <Text style={styles.textNB}>Description: {JSON.stringify(description).replace(/"/g,"")}</Text>
+                
+                {/* buttons */}
+                <View style={{ flexDirection:"row" }}>
+                        <TouchableOpacity style={[styles.buttonSmall, {opacity: registeredButton ? 0.3 : 1}]}
+                        disabled ={registeredButton} onPress={() => onLike()}>
+                                <Text style={styles.buttonSmallText}>Like</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.buttonSmall, {opacity: registeredButton ? 0.3 : 1}]} 
+                        disabled ={registeredButton} onPress={() => {navigation.navigate('Create Reply', {title : title})}}>
+                                <Text style={styles.buttonSmallText}>Comment</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonSmall} onPress={() => onShare()}>
+                                <Text style={styles.buttonSmallText}>Share</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                {/* FlatList -- replies */}
+                
+                    <FlatList
+                        data={reply}
+                        renderItem={ItemView}
+                        keyExtractor={(item, index) => index.toString()}
+                    />
+                    </ScrollView>
+            </View>
+        )
+            } else {
             
-                <FlatList
-                    data={reply}
-                    renderItem={ItemView}
-                    keyExtractor={(item, index) => index.toString()}
-                />
-                </ScrollView>
-        </View>
-    )
+                return(
+                    <View style={styles.threadContainer}>
+                        <ScrollView scrollIndicatorInsets={{ top: 1, bottom: 1 }}>
+            
+                        {/* Header */}
+                        <Text style={styles.Heading}>{JSON.stringify(title).replace(/"/g,"")}</Text>
+            
+                        {/* buttons */}
+                        <View style={{ flexDirection:"row"}}>
+                    </View>
+            
+                        {/* details */}
+                        <Text style={styles.textNB}>Title: {JSON.stringify(title).replace(/"/g,"")}</Text>
+                        <Text style={styles.textNB}>Section: {JSON.stringify(section).replace(/"/g,"")}</Text>
+                        <Text style={styles.textNB}>Description: {JSON.stringify(description).replace(/"/g,"")}</Text>
+                        
+                        {/* buttons */}
+                        <View style={{ flexDirection:"row" }}>
+                                <TouchableOpacity style={[styles.buttonSmall, {opacity: registeredButton ? 0.3 : 1}]}
+                                disabled ={registeredButton} onPress={() => onLike()}>
+                                        <Text style={styles.buttonSmallText}>Like</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={[styles.buttonSmall, {opacity: registeredButton ? 0.3 : 1}]} 
+                                disabled ={registeredButton} onPress={() => {navigation.navigate('Create Reply', {title : title})}}>
+                                        <Text style={styles.buttonSmallText}>Comment</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={styles.buttonSmall} onPress={() => onShare()}>
+                                        <Text style={styles.buttonSmallText}>Share</Text>
+                                </TouchableOpacity>
+                            </View>
+            
+                        {/* FlatList -- replies */}
+                        
+                            <FlatList
+                                data={reply}
+                                renderItem={ItemView}
+                                keyExtractor={(item, index) => index.toString()}
+                            />
+                            </ScrollView>
+                    </View>
+                )
+            }
 }
